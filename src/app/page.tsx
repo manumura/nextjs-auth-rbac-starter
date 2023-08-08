f88e854df37a9e265c8335aea0c47c6e77a314eb
Emmanuel Mura <emmanuel.mura@gmail.com>
2023-08-08 19:13:02 +0700

    catch fetch error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,28 +6,31 @@ async function getAppInfos() {
   let message = 'Welcome fallback!';
   let information = {};
 
-  const baseUrl = getClientBaseUrl(headers());
+  try {
+    const baseUrl = getClientBaseUrl(headers());
+    const welcomeRes = await fetch(`${baseUrl}/api/welcome`, {
+      method: 'GET',
+    });
 
-  const welcomeRes = await fetch(`${baseUrl}/api/welcome`, {
-    method: 'GET',
-  });
+    if (welcomeRes.ok) {
+      const welcomeAsJson = await welcomeRes.json();
+      message = welcomeAsJson.message;
+    }
 
-  if (welcomeRes.ok) {
-    const welcomeAsJson = await welcomeRes.json();
-    message = welcomeAsJson.message;
+    const infoRes = await fetch(`${baseUrl}/api/info`, {
+      method: 'GET',
+    });
+
+    if (infoRes.ok) {
+      information = await infoRes.json();
+    }
+    // const welcomeRes = await welcome();
+    // message = welcomeRes.data;
+    // const infoRes = await info();
+    // information = infoRes.data;
+  } catch (error) {
+    console.error(error);
   }
-
-  const infoRes = await fetch(`${baseUrl}/api/info`, {
-    method: 'GET',
-  });
-
-  if (infoRes.ok) {
-    information = await infoRes.json();
-  }
-  // const welcomeRes = await welcome();
-  // message = welcomeRes.data;
-  // const infoRes = await info();
-  // information = infoRes.data;
 
   return {
     message,
