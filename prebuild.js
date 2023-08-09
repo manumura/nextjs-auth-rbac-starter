b7816f95838f3b685e3394f02f84ed3116d86d80
Emmanuel Mura <emmanuel.mura@gmail.com>
2023-08-09 19:52:40 +0700

    prebuild script

--- prebuild.js
+++ prebuild.js
@@ -1,22 +1,24 @@
-console.log("********* PREBUILDING");
-const path = require("node:path");
-const fs = require("fs");
+console.log('********* PREBUILDING');
+const path = require('node:path');
+const fs = require('fs');
 const baseDir = process.cwd();
+
+// /node_modules/@netlify/plugin-nextjs/lib/templates/requireHooks.js
 
 const prebuildScripts = async () => {
   const file = path.join(
     baseDir,
-    "/node_modules",
-    "next/dist/server/require-hook.js"
+    '/node_modules',
+    '@netlify/plugin-nextjs/lib/templates/requireHooks.js',
   );
 
-  const content = await fs.promises.readFile(file, "utf-8");
+  const content = await fs.promises.readFile(file, 'utf-8');
   await fs.promises.writeFile(
     file,
     content.replace(
-      "if (process.env.__NEXT_PRIVATE_PREBUNDLED_REACT) {",
-      "if (true) {"
-    )
+      "const reactMode = process.env.__NEXT_PRIVATE_PREBUNDLED_REACT || 'default';",
+      "const reactMode = process.env.__NEXT_PRIVATE_PREBUNDLED_REACT || 'next';",
+    ),
   );
 };
 
