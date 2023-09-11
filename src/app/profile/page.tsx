bd7bd1401bf4374f0ece255d9a6d71e3f45bafa4
Emmanuel Mura <emmanuel.mura@gmail.com>
2023-09-11 17:36:16 +0700

    add logs for prod

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -17,6 +17,7 @@ async function getProfile(): Promise<IUser | undefined> {
   });
 
   console.log('res get profile: ', res);
+  console.log('res.ok get profile: ', res.ok);
 
   if (!res.ok) {
     console.error(`Get Profile getServerSideProps error: ${res.statusText}`);
