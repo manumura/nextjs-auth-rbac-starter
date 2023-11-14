9a65bebc990cf93bf15e7ae75b4bf2d157f88403
Emmanuel Mura <emmanuel.mura@gmail.com>
2023-11-14 08:56:24 +0700

    update dependencies

--- next.config.js
+++ next.config.js
@@ -2,8 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['res.cloudinary.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        pathname: '**',
+      },
+    ],
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
