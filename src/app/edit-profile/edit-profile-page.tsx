f13146529e88e2dc9228c2019ddc196a5c34a170
Emmanuel Mura <emmanuel.mura@gmail.com>
2024-03-10 18:14:19 +0700

    edit profile typo

--- src/app/edit-profile/edit-profile-page.tsx
+++ src/app/edit-profile/edit-profile-page.tsx
@@ -163,7 +163,7 @@ export default function EditProfilePage({ user }) {
     } finally {
       setLoading(false);
     }
-  }
+  };
   // ------------------------------------------------
 
   const nameConstraints = {
