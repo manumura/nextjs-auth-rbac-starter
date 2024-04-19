bd9d490a7d9919c4afeb83a4ac6f7e487247a4e5
Emmanuel Mura <emmanuel.mura@gmail.com>
2024-04-19 16:20:36 +0700

    update dependencies + eslint prettier

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -65,7 +65,7 @@ export default async function RootLayout({
   // This will be populated with nested layouts or pages
   children,
 }: {
-  children: React.ReactNode;
+  readonly children: React.ReactNode;
 }) {
   const user = await getProfile();
   const navItems = getNavItems(user);
