9e1a34f3e866242429eb541a0fa5c5eb7298e844
Emmanuel Mura <emmanuel.mura@gmail.com>
2024-12-27 13:43:55 +0700

    update dependencies

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -5,7 +5,7 @@ import { GoogleReCaptchaProvider } from 'react-google-recaptcha-v3';
 import { ToastContainer } from 'react-toastify';
 
 // To avoid tailwind to purge toastify styles
-import 'react-toastify/dist/ReactToastify.min.css';
+// import 'react-toastify/dist/ReactToastify.min.css';
 import appConfig from '../config/config';
 
 export function Providers({ children }) {
