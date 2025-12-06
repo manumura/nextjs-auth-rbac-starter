439fc7027ffbf5f6e4b4c971b5f3fdeaa4589a0b
manumura <emmanuel.mura@gmail.com>
2025-12-06 21:34:51 +0700

    upgrade eslint 9

---
+++ eslint.config.mjs
@@ -0,0 +1,18 @@
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import prettier from 'eslint-config-prettier/flat';
+import { defineConfig, globalIgnores } from 'eslint/config';
+
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  prettier,
+  // Override default ignores of eslint-config-next.
+  globalIgnores([
+    // Default ignores of eslint-config-next:
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
+  ]),
+]);
+
+export default eslintConfig;
